e242873326b8c0f96cea15798e6d0b230ba34c61
Мачкалян Тигран Норайрович <m2305719@edu.misis.ru>
2023-09-26 10:10:00 +0300

    AAA

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test2");
+            Console.WriteLine("Test3");
         }
     }
 }
