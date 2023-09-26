037da60883afce87201c3e4deceafebadef8f5d5
Tigran Machkalyan <tigranmachkalyan@mail.ru>
2023-09-26 22:52:06 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleApp1
 {
     internal class Program
     {
+        static public void Level1()
+        {
+            double s = 0;
+            
+            //1
+            for(int i = 2; i <= 35; i += 3) {
+                s += i;
+            }
+            Console.WriteLine("1_1:\n{0}", s);
+
+            //2
+            s = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                s += 1/i;
+            }
+            Console.WriteLine("1_2:\n{0}", s);
+        }
+           
         static void Main(string[] args)
         {
-            Console.WriteLine("test from home");
+            Level1();
         }
     }
 }
