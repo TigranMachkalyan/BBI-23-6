f8659bd43e8ec7d295c03ced099109bed68ceabc
TigranMachkalyan <146061716+TigranMachkalyan@users.noreply.github.com>
2023-09-26 10:06:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test1");
+            Console.WriteLine("Test2");
         }
     }
 }
